8ad9c0787a4728bf06cf87239e3ced1dcec3ae00
Oscar <oscar_wika@hotmail.com>
2025-05-07 13:14:12 -0700

    revert?

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -45,7 +45,7 @@ document.addEventListener('DOMContentLoaded', function() {
             padding: 20px;
         }
         .main-carousel {
-            margin-bottom: 10px;
+            margin-bottom: 20px;
         }
         .splide__slide img {
             width: 100%;
